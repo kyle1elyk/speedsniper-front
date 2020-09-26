91e27098f34cb7ee989e38ca60ba3c15418de0cc
kyle1elyk <steadkyle@gmail.com>
2020-09-26 16:21:09 -0400

    Testing env

--- api/scans/index.js
+++ api/scans/index.js
@@ -1,6 +1,15 @@
 module.exports = async function (context, req) {
     
     const scans = [{plate:"XXX", color:"#000000", speed:18.7, time:1601148057}];
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "yourusername",
+        password: "yourpassword"
+      });
+
+    console.log(process.env.SQLConnectionString);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
